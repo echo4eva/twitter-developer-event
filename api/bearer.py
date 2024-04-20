c9630119e0790c0dcd693b09b369ae5c9ddafaf5
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-04-20 21:58:27 +0000

    push

---
+++ api/bearer.py
@@ -0,0 +1,11 @@
+import tweepy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+auth = tweepy.OAuth2AppHandler(
+    os.getenv("TWITTER_API_KEY"),
+    os.getenv("TWITTER_API_SECRET")
+)
+api = tweepy.API(auth)
---
+++ api/client.py
@@ -0,0 +1,12 @@
+import tweepy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+oauth2_user_handler = tweepy.OAuth2UserHandler(
+    client_id=os.getenv("TWITTER_CLIENT_ID"),
+    redirect_uri=os.getenv("TWITTER_REDIRECT_URI"),
+    scope=os.getenv("TWITTER_SCOPES").split(","),
+    client_secret=os.getenv("TWITTER_CLIENT_SECRET")
+)
