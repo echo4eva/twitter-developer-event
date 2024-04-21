147bd4e2ea275bbb767e816c32c1e4b9ae595b08
Khalid Al Jughaiman <kbj0@berkeley.edu>
2024-04-21 09:19:53 -0700

    grok functions implemented

--- api/grok/grok_single_question.py
+++ api/grok/grok_single_question.py
@@ -1,32 +1,27 @@
 import asyncio
-import sys
 import xai_sdk
 import os
 
 os.environ['XAI_API_KEY'] = 'Eh97MbeIZ4p4UjhF4D8JVyTRAZm7oErMkdePDVi1jWzNYWPq47XPUFWgqcBd0Ysa7bfaAwrHZCVxK+pzGSVBaXUvHmKzZ8F34vsqwtDpI3hKBCf3rhIz/Obwir0obKZ9PQ'
+
 
 class GrokWrapper:
       def __init__(self):
           self.client = xai_sdk.Client()
           self.grok = self.client.grok
 
-      async def chat(self, input_text):
+      async def ask_question(self, question):
           conversation = self.grok.create_conversation()
-          print("New conversation created.")
-
-          token_stream, _ = conversation.add_response(input_text)
-          print("Grok: ", end="")
+          token_stream, _ = conversation.add_response(question)
           response = ""
           async for token in token_stream:
               response += token
-              sys.stdout.flush()
-          print("\n")
           return response.strip()
 
 async def main():
       wrapper = GrokWrapper()
-      input_text = input("Enter your message: ")
-      response = await wrapper.chat(input_text)
+      question = input("Enter your question: ")
+      response = await wrapper.ask_question(question)
       print("Grok's response:", response)
 
 if __name__ == "__main__":
